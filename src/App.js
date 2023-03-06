8106ea88245598e1cf6f2ff59a35c17e1fdc4abd
Manipal <maniabhi149@gmail.com>
2023-03-06 19:52:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './app.css';
+import Table from './Table';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello Manipal welcome to React.js Library</h2>
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
+
